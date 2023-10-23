94b36df88a8505784444d1913fba4fcc95a23648
Matt Eland <Matt.Eland@GMail.com>
2023-10-23 19:54:30 -0400

    Added secrets files

--- LinkedIn.AIEngineer.Vision/Program.cs
+++ LinkedIn.AIEngineer.Vision/Program.cs
@@ -1,5 +1,19 @@
-﻿internal class Program {
-    private static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+
+internal class Program {
+    private static void Main() {
+
+        // Read settings from Settings.json and environment variable
+        IConfigurationRoot configurationRoot = new ConfigurationBuilder()
+            .AddJsonFile("Settings.json", optional: true, reloadOnChange: true)
+            .AddUserSecrets(Assembly.GetExecutingAssembly(), optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        // Get the computer vision setting from the configuration
+        string visionKey = configurationRoot["VisionKey"]!;
+
+        Console.WriteLine("Vision Key: {0}", visionKey);
     }
 }
